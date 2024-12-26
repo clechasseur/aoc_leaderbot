5111c32e3d6cf3b1b457cf9138d3fea71f368981
Charles Lechasseur <shiftingbeard@outlook.com>
2024-12-26 13:25:23 -0500

    fix: try to fix build and MSRV errors, introduce build script

--- aoc_leaderboard/src/lib.rs
+++ aoc_leaderboard/src/lib.rs
@@ -1,15 +1,24 @@
 //! A crate with a strongly-typed wrapper for an [Advent of Code] leaderboard,
 //! along with ways to fetch them from the AoC website.
+//! 
+//! This crate's API consists essentially of the [`Leaderboard`] type and its
+//! related subcomponents. If the `http` feature is enabled, a helper to fetch
+//! a leaderboard's data from the Advent of Code website is also provided.
 //!
 //! [Advent of Code]: https://adventofcode.com/
+//! [`Leaderboard`]: aoc::Leaderboard
 
 #![deny(missing_docs)]
 #![deny(rustdoc::missing_crate_level_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
 #![deny(rustdoc::private_intra_doc_links)]
+#![cfg_attr(any(nightly_rustc, docsrs), feature(doc_cfg))]
 
 pub mod aoc;
 pub mod error;
 
 pub use error::Error;
 pub use error::Result;
+
+#[doc(hidden)]
+pub use reqwest;
