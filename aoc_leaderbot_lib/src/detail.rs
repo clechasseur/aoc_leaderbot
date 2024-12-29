554d88712af096c1e7448747ca1c92f2668477b6
Charles Lechasseur <shiftingbeard@outlook.com>
2024-12-29 09:45:57 -0500

    chore: add tests

--- aoc_leaderbot_lib/src/detail.rs
+++ aoc_leaderbot_lib/src/detail.rs
@@ -10,7 +10,7 @@ where
     K: AsRef<OsStr>,
 {
     let key = key.as_ref();
-    
+
     env::var(key).map_err(|err| crate::Error::Env {
         var_name: key.to_string_lossy().into(),
         source: err.into(),
@@ -24,7 +24,7 @@ where
 {
     let key = key.as_ref();
     let actual = env_var(key)?;
-    
+
     actual.parse().map_err(|source| crate::Error::Env {
         var_name: key.to_string_lossy().into(),
         source: EnvVarError::IntExpected { actual, source },
@@ -33,41 +33,73 @@ where
 
 #[cfg(test)]
 mod tests {
+    use assert_matches::assert_matches;
+    use uuid::Uuid;
+
     use super::*;
-    
-    mod int_env_var {
-        use assert_matches::assert_matches;
-        use serial_test::serial;
+
+    fn random_var_name() -> String {
+        format!("test_{}", Uuid::new_v4())
+    }
+
+    mod env_var {
         use super::*;
-        
-        const TEST_ENV_VAR: &str = "INT_ENV_VAR_TEST_ENV_VAR";
-        
+
         #[test]
-        #[serial(int_env_var)]
+        fn valid() {
+            let test_var_name = random_var_name();
+            env::set_var(&test_var_name, "foo");
+
+            let actual = env_var(&test_var_name);
+            assert_matches!(actual, Ok(value) if value == "foo");
+        }
+
+        #[test]
+        fn not_present() {
+            let test_var_name = random_var_name();
+            env::remove_var(&test_var_name); // Just in case 😉
+
+            let actual = env_var(&test_var_name);
+            assert_matches!(actual, Err(crate::Error::Env { var_name, source }) => {
+                assert_eq!(var_name, test_var_name);
+                assert_matches!(source, EnvVarError::NotPresent);
+            })
+        }
+    }
+
+    mod int_env_var {
+        use super::*;
+
+        #[test]
         fn valid_int() {
-            env::set_var(TEST_ENV_VAR, "42");
-            
-            let actual = int_env_var::<i32, _>(TEST_ENV_VAR);
+            let test_var_name = random_var_name();
+            env::set_var(&test_var_name, "42");
+
+            let actual = int_env_var::<i32, _>(&test_var_name);
             assert_matches!(actual, Ok(42));
         }
-        
+
         #[test]
-        #[serial(int_env_var)]
         fn not_present() {
-            env::remove_var(TEST_ENV_VAR);
-            
-            let actual = int_env_var::<i32, _>(TEST_ENV_VAR);
-            assert_matches!(actual, Err(crate::Error::Env(EnvVarError::NotPresent)));
+            let test_var_name = random_var_name();
+            env::remove_var(&test_var_name); // Just in case 😉
+
+            let actual = int_env_var::<i32, _>(&test_var_name);
+            assert_matches!(actual, Err(crate::Error::Env { var_name, source }) => {
+                assert_eq!(var_name, test_var_name);
+                assert_matches!(source, EnvVarError::NotPresent);
+            });
         }
 
         #[test]
-        #[serial(int_env_var)]
         fn invalid_int() {
-            env::set_var(TEST_ENV_VAR, "forty-two");
+            let test_var_name = random_var_name();
+            env::set_var(&test_var_name, "forty-two");
 
-            let actual = int_env_var::<i32, _>(TEST_ENV_VAR);
-            assert_matches!(actual, Err(crate::Error::Env(EnvVarError::IntExpected { actual, .. })) => {
-                assert_eq!(actual, "forty-two");
+            let actual = int_env_var::<i32, _>(&test_var_name);
+            assert_matches!(actual, Err(crate::Error::Env { var_name, source }) => {
+                assert_eq!(var_name, test_var_name);
+                assert_matches!(source, EnvVarError::IntExpected { actual, .. } if actual == "forty-two");
             });
         }
     }
--- aoc_leaderbot_lib/src/leaderbot/config.rs
+++ aoc_leaderbot_lib/src/leaderbot/config.rs
@@ -1,15 +1,17 @@
 //! Implementations of [`LeaderbotConfig`](crate::leaderbot::LeaderbotConfig).
 
-pub use config_static::*;
 pub use config_env::*;
+pub use config_static::*;
 
 #[cfg(feature = "config-static")]
 #[cfg_attr(any(nightly_rustc, docsrs), doc(cfg(feature = "config-static")))]
 mod config_static {
     use std::any::type_name;
+
     use chrono::{Datelike, Local};
     use derive_builder::{Builder, UninitializedFieldError};
     use serde::{Deserialize, Serialize};
+
     use crate::leaderbot::LeaderbotConfig;
 
     /// Static bot config with predefined values.
@@ -89,12 +91,21 @@ mod config_static {
 #[cfg(feature = "config-env")]
 #[cfg_attr(any(nightly_rustc, docsrs), doc(cfg(feature = "config-env")))]
 mod config_env {
-    use std::env;
+    use std::fmt::Debug;
+
     use super::*;
-    
-    use crate::detail::int_env_var;
+    use crate::detail::{env_var, int_env_var};
     use crate::error::EnvVarError;
     use crate::leaderbot::LeaderbotConfig;
+
+    /// Environment variable name suffix for `year`. See [`get_env_config`].
+    pub const ENV_CONFIG_YEAR_SUFFIX: &str = "YEAR";
+
+    /// Environment variable name suffix for `leaderboard_id`. See [`get_env_config`].
+    pub const ENV_CONFIG_LEADERBOARD_ID_SUFFIX: &str = "LEADERBOARD_ID";
+
+    /// Environment variable name suffix for `aoc_session`. See [`get_env_config`].
+    pub const ENV_CONFIG_AOC_SESSION_SUFFIX: &str = "AOC_SESSION";
 
     /// Loads bot config values from the environment.
     ///
@@ -105,16 +116,18 @@ mod config_env {
     /// | `{prefix}YEAR`           | `year`           | Current year  |
     /// | `{prefix}LEADERBOARD_ID` | `leaderboard_id` | -             |
     /// | `{prefix}AOC_SESSION`    | `aoc_session`    | -             |
-    pub fn get_env_config<S>(env_var_prefix: S) -> crate::Result<impl LeaderbotConfig + Send>
+    pub fn get_env_config<S>(
+        env_var_prefix: S,
+    ) -> crate::Result<impl LeaderbotConfig + Send + Debug>
     where
         S: AsRef<str>,
     {
         let env_var_prefix = env_var_prefix.as_ref();
         let var_name = |name| format!("{env_var_prefix}{name}");
 
-        let year = match int_env_var(var_name("YEAR")) {
+        let year = match int_env_var(var_name(ENV_CONFIG_YEAR_SUFFIX)) {
             Ok(year) => Some(year),
-            Err(crate::Error::Env(EnvVarError::NotPresent)) => None,
+            Err(crate::Error::Env { source: EnvVarError::NotPresent, .. }) => None,
             Err(err) => return Err(err),
         };
 
@@ -123,24 +136,26 @@ mod config_env {
             builder.year(year);
         }
         builder
-            .leaderboard_id(int_env_var(var_name("LEADERBOARD_ID"))?)
-            .aoc_session(env::var(var_name("AOC_SESSION"))?)
+            .leaderboard_id(int_env_var(var_name(ENV_CONFIG_LEADERBOARD_ID_SUFFIX))?)
+            .aoc_session(env_var(var_name(ENV_CONFIG_AOC_SESSION_SUFFIX))?)
             .build()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::any::type_name;
     use assert_matches::assert_matches;
     use chrono::{Datelike, Local};
-    use crate::leaderbot::LeaderbotConfig;
+
     use super::*;
+    use crate::leaderbot::LeaderbotConfig;
 
     #[cfg(feature = "config-static")]
     mod static_leaderbot_config {
+        use std::any::type_name;
+
         use super::*;
-        
+
         #[test]
         fn new() {
             let actual = StaticLeaderbotConfig::new(2024, 12345, "aoc_session");
@@ -204,6 +219,122 @@ mod tests {
                     assert_eq!(target, type_name::<StaticLeaderbotConfig>());
                     assert_eq!(field, "aoc_session");
                 });
+            }
+        }
+    }
+
+    #[cfg(feature = "config-env")]
+    mod get_env_config {
+        use std::env;
+
+        use uuid::Uuid;
+
+        use super::*;
+        use crate::error::EnvVarError;
+
+        fn random_env_var_prefix() -> String {
+            format!("test_{}_", Uuid::new_v4())
+        }
+
+        fn perform_valid_test(env_var_prefix: &str, set_year: bool) {
+            let var_name = |name| format!("{env_var_prefix}{name}");
+
+            if set_year {
+                env::set_var(var_name(ENV_CONFIG_YEAR_SUFFIX), "2024");
+            }
+            env::set_var(var_name(ENV_CONFIG_LEADERBOARD_ID_SUFFIX), "12345");
+            env::set_var(var_name(ENV_CONFIG_AOC_SESSION_SUFFIX), "aoc_session");
+
+            let actual = get_env_config(env_var_prefix).unwrap();
+
+            assert_eq!(actual.year(), if set_year { 2024 } else { Local::now().year() });
+            assert_eq!(actual.leaderboard_id(), 12345);
+            assert_eq!(actual.aoc_session(), "aoc_session");
+        }
+
+        mod empty_prefix {
+            use super::*;
+
+            #[test]
+            fn with_year() {
+                perform_valid_test("", true);
+            }
+
+            #[test]
+            fn without_year() {
+                perform_valid_test("", false);
+            }
+        }
+
+        mod non_empty_prefix {
+            use super::*;
+
+            #[test]
+            fn with_year() {
+                perform_valid_test(&random_env_var_prefix(), true);
+            }
+
+            #[test]
+            fn without_year() {
+                perform_valid_test(&random_env_var_prefix(), false);
+            }
+        }
+
+        mod missing_vars {
+            use super::*;
+
+            #[test]
+            fn missing_leaderboard_id() {
+                let env_var_prefix = random_env_var_prefix();
+                let var_name = |name| format!("{env_var_prefix}{name}");
+
+                env::set_var(var_name(ENV_CONFIG_AOC_SESSION_SUFFIX), "aoc_session");
+
+                let actual = get_env_config(&env_var_prefix);
+                assert_matches!(actual, Err(crate::Error::Env { var_name: actual_var_name, source: EnvVarError::NotPresent }) if actual_var_name == var_name(ENV_CONFIG_LEADERBOARD_ID_SUFFIX));
+            }
+
+            #[test]
+            fn missing_aoc_session() {
+                let env_var_prefix = random_env_var_prefix();
+                let var_name = |name| format!("{env_var_prefix}{name}");
+
+                env::set_var(var_name(ENV_CONFIG_LEADERBOARD_ID_SUFFIX), "12345");
+
+                let actual = get_env_config(&env_var_prefix);
+                assert_matches!(actual, Err(crate::Error::Env { var_name: actual_var_name, source: EnvVarError::NotPresent }) if actual_var_name == var_name(ENV_CONFIG_AOC_SESSION_SUFFIX));
+            }
+        }
+
+        mod invalid_values {
+            use super::*;
+
+            #[test]
+            fn invalid_year() {
+                let env_var_prefix = random_env_var_prefix();
+                let var_name = |name| format!("{env_var_prefix}{name}");
+
+                env::set_var(var_name(ENV_CONFIG_YEAR_SUFFIX), "two-thousand-twenty-four");
+
+                let actual = get_env_config(&env_var_prefix);
+                assert_matches!(actual, Err(crate::Error::Env { var_name: actual_var_name, source }) => {
+                    assert_eq!(actual_var_name, var_name(ENV_CONFIG_YEAR_SUFFIX));
+                    assert_matches!(source, EnvVarError::IntExpected { actual, .. } if actual == "two-thousand-twenty-four");
+                })
+            }
+
+            #[test]
+            fn invalid_leaderboard_id() {
+                let env_var_prefix = random_env_var_prefix();
+                let var_name = |name| format!("{env_var_prefix}{name}");
+
+                env::set_var(var_name(ENV_CONFIG_LEADERBOARD_ID_SUFFIX), "one two three four five");
+
+                let actual = get_env_config(&env_var_prefix);
+                assert_matches!(actual, Err(crate::Error::Env { var_name: actual_var_name, source }) => {
+                    assert_eq!(actual_var_name, var_name(ENV_CONFIG_LEADERBOARD_ID_SUFFIX));
+                    assert_matches!(source, EnvVarError::IntExpected { actual, .. } if actual == "one two three four five");
+                })
             }
         }
     }
