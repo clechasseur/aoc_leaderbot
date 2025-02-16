7e590896d69e7325a6e7b53da3fc07cceacc052f
Charles Lechasseur <shiftingbeard@outlook.com>
2025-02-16 01:02:40 -0500

    chore: more documentation

--- aoc_leaderbot_aws_lib/src/lib.rs
+++ aoc_leaderbot_aws_lib/src/lib.rs
@@ -2,10 +2,25 @@
 //! an [Advent of Code] private leaderboard for changes and report them to various channels
 //! like Slack.
 //!
-//! This library is mostly internal, so it provides no guarantee on API stability.
+//! ## Trait implementations
+//!
+//! This library includes implementations of the traits found in [`aoc_leaderbot_lib`].
+//!
+//! ### [`DynamoDbStorage`]
+//!
+//! Required feature: `storage-dynamodb` (enabled by default)
+//!
+//! An implementation of the [`Storage`] trait that stores data in an [AWS DynamoDB] table.
+//!
+//! The only thing that the storage needs is the name of the table where to store data.
+//! If that table does not yet exist, it's possible to create it via the [`create_table`].
 //!
 //! [`aoc_leaderbot`]: https://github.com/clechasseur/aoc_leaderbot
 //! [Advent of Code]: https://adventofcode.com/
+//! [`DynamoDbStorage`]: leaderbot::storage::aws::dynamodb::DynamoDbStorage
+//! [`Storage`]: aoc_leaderbot_lib::leaderbot::Storage
+//! [AWS DynamoDB]: https://aws.amazon.com/dynamodb/
+//! [`create_table`]: leaderbot::storage::aws::dynamodb::DynamoDbStorage::create_table
 
 #![deny(missing_docs)]
 #![deny(rustdoc::missing_crate_level_docs)]
