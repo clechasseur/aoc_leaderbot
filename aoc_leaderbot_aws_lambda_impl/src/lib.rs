a3c74faf7114ba52b56b9ef9b60d2cb014665874
Charles Lechasseur <shiftingbeard@outlook.com>
2025-02-24 08:51:59 -0500

    chore: add testing feature to lambda impl project

--- aoc_leaderbot_aws_lambda_impl/src/lib.rs
+++ aoc_leaderbot_aws_lambda_impl/src/lib.rs
@@ -10,6 +10,7 @@
 #![deny(rustdoc::missing_crate_level_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
 #![deny(rustdoc::private_intra_doc_links)]
+#![cfg_attr(any(nightly_rustc, docsrs), feature(doc_cfg))]
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
 pub mod leaderbot;
--- aoc_leaderbot_aws_lambda_impl/tests/leaderbot.rs
+++ aoc_leaderbot_aws_lambda_impl/tests/leaderbot.rs
@@ -1,3 +1,5 @@
-// These tests require Docker, which only seems to work reliably on Linux in GitHub workflows.
-#[cfg(any(not(ci), target_os = "linux"))]
+#[cfg(all(
+    any(not(ci), target_os = "linux"), // These tests require Docker, which only seems to work reliably on Linux in GitHub workflows.
+    feature = "__testing"              // These tests only work if you compile with the internal `__testing` feature.
+))]
 mod bot_lambda_handler {}
