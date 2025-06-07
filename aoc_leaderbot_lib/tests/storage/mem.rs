a5897a64964640b37e9dc357cf063f5258f247c0
Charles Lechasseur <shiftingbeard@outlook.com>
2025-06-07 02:06:09 -0400

    chore: add tests

--- aoc_leaderbot_lib/tests/storage/mem.rs
+++ aoc_leaderbot_lib/tests/storage/mem.rs
@@ -3,6 +3,8 @@ mod memory_storage {
     use aoc_leaderboard::test_helpers::{test_leaderboard, TEST_LEADERBOARD_ID, TEST_YEAR};
     use aoc_leaderbot_lib::leaderbot::storage::mem::MemoryStorage;
     use aoc_leaderbot_lib::leaderbot::Storage;
+    use aoc_leaderbot_lib::ErrorKind;
+    use assert_matches::assert_matches;
     use rstest::rstest;
 
     mod new {
@@ -90,6 +92,67 @@ mod memory_storage {
                 .unwrap();
             assert!(previous_leaderboard.is_none());
             assert!(previous_error.is_none());
+        }
+
+        #[rstest]
+        #[test_log::test(tokio::test)]
+        async fn save_success_and_error(
+            #[from(test_leaderboard)] leaderboard: Leaderboard,
+            #[from(test_leaderboard)] expected: Leaderboard,
+        ) {
+            let mut storage = MemoryStorage::new();
+
+            let (previous_leaderboard, previous_error) = storage
+                .load_previous(TEST_YEAR, TEST_LEADERBOARD_ID)
+                .await
+                .unwrap();
+            assert!(previous_leaderboard.is_none());
+            assert!(previous_error.is_none());
+
+            let error_kind = ErrorKind::Leaderboard(aoc_leaderboard::ErrorKind::NoAccess);
+            storage
+                .save_error(TEST_YEAR, TEST_LEADERBOARD_ID, error_kind)
+                .await
+                .unwrap();
+
+            let (previous_leaderboard, previous_error) = storage
+                .load_previous(TEST_YEAR, TEST_LEADERBOARD_ID)
+                .await
+                .unwrap();
+            assert!(previous_leaderboard.is_none());
+            assert_matches!(previous_error, Some(previous_err) => {
+                assert_eq!(error_kind, previous_err);
+            });
+
+            storage
+                .save_success(TEST_YEAR, TEST_LEADERBOARD_ID, &leaderboard)
+                .await
+                .unwrap();
+
+            let (previous_leaderboard, previous_error) = storage
+                .load_previous(TEST_YEAR, TEST_LEADERBOARD_ID)
+                .await
+                .unwrap();
+            assert_matches!(previous_leaderboard, Some(previous_leaderboard) => {
+                assert_eq!(previous_leaderboard, expected);
+            });
+            assert!(previous_error.is_none());
+
+            storage
+                .save_error(TEST_YEAR, TEST_LEADERBOARD_ID, error_kind)
+                .await
+                .unwrap();
+
+            let (previous_leaderboard, previous_error) = storage
+                .load_previous(TEST_YEAR, TEST_LEADERBOARD_ID)
+                .await
+                .unwrap();
+            assert_matches!(previous_leaderboard, Some(previous_leaderboard) => {
+                assert_eq!(previous_leaderboard, expected);
+            });
+            assert_matches!(previous_error, Some(previous_err) => {
+                assert_eq!(error_kind, previous_err);
+            });
         }
     }
 }
