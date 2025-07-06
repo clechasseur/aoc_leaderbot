eee5bef1e450de5c9ae2ce68c8a5a5f1cebf3947
Charles Lechasseur <shiftingbeard@outlook.com>
2025-07-06 03:15:34 -0400

    feat: add AWS Lambda installer bin crate

--- aoc_leaderbot_aws_lambda/src/lib.rs
+++ aoc_leaderbot_aws_lambda/src/lib.rs
@@ -1,8 +1,8 @@
-//! Library implementing an [AWS Lambda] handler version of [`aoc_leaderbot`], a bot that can watch
-//! an [Advent of Code] private leaderboard for changes and report them to various channels
-//! like Slack.
+//! Library implementing utilities required to deploy the [AWS Lambda] version of [`aoc_leaderbot`]
+//! to an AWS account. `aoc_leaderbot` is a bot that can watch an [Advent of Code] private
+//! leaderboard for changes and report them to various channels like Slack.
 //!
-//! This library is not meant to be used outside the lambda handler binary and makes no guarantee
+//! This library is not meant to be used outside the installer binary and makes no guarantee
 //! on API stability. For more information on installing, see [the project page].
 //!
 //! [AWS Lambda]: https://aws.amazon.com/lambda/
@@ -14,7 +14,5 @@
 #![deny(rustdoc::missing_crate_level_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
 #![deny(rustdoc::private_intra_doc_links)]
-#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg_hide))]
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
-
-pub mod leaderbot;
