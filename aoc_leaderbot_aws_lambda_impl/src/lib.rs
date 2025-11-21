b4bd1b0778f9afe854bd3cc6b9b133ec5e866845
Charles Lechasseur <shiftingbeard@outlook.com>
2025-11-21 06:58:04 -0500

    chore: post-rebase cleanup

--- aoc_leaderbot_aws_lambda_impl/src/lib.rs
+++ aoc_leaderbot_aws_lambda_impl/src/lib.rs
@@ -2,13 +2,9 @@
 //! an [Advent of Code] private leaderboard for changes and report them to various channels
 //! like Slack.
 //!
-//! This library is not meant to be used outside the lambda handler binary and makes no guarantee
-//! on API stability. For more information on installing, see [the project page].
-//!
 //! [AWS Lambda]: https://aws.amazon.com/lambda/
 //! [`aoc_leaderbot`]: https://github.com/clechasseur/aoc_leaderbot
 //! [Advent of Code]: https://adventofcode.com/
-//! [the project page]: https://github.com/clechasseur/aoc_leaderbot
 
 #![deny(missing_docs)]
 #![deny(rustdoc::missing_crate_level_docs)]
